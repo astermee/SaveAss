9c94f7ee039ff06bf8be311a7fc97d2ebacb6c96
Полина <belova.pauline98@gmail.com>
2023-02-11 14:53:48 +0500

    Add comments

--- very-easy-tasks/src/org/com/very/easy/tasks/task/Task8.java
+++ very-easy-tasks/src/org/com/very/easy/tasks/task/Task8.java
@@ -10,8 +10,10 @@ public class Task8 extends Task {
     @Override
     public void run() {
         super.run();
+        //создаём обычный массив чисел с заданными элементами внутри, которые указываются внутри фиг. скобок {}
         int[] arr = {1, 2, 3, 4, 5};
         System.out.print("Ответ: ");
+        //для печати массива можно воспользоваться циклом или биб-кой Arrays, как в следующем задании
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
