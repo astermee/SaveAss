fbb37626970c1fead43765bdf894fd9241e983a5
Полина <belova.pauline98@gmail.com>
2023-02-11 14:30:09 +0500

    Add comments

--- very-easy-tasks/src/org/com/very/easy/tasks/task/Task1.java
+++ very-easy-tasks/src/org/com/very/easy/tasks/task/Task1.java
@@ -1,14 +1,16 @@
 package org.com.very.easy.tasks.task;
 
+//наследуемся от абстрактноого класса Task, чтобы пользоваться его содержимым
 public class Task1 extends Task {
-
+    //метод таск1, который пользуется сеттерами номера и описания в классе Task.
     public Task1() {
         setNumber(1);
         setDescription("Напишите программу, которая выводит на консоль строку 'Hello, World!'.");
     }
-
+    //аннотация с указанием того, что последующее после него перенаследовано,в нашем случае из класса Task
     @Override
     public void run() {
+        //срабатывает метод run из Task https://www.examclouds.com/ru/java/java-core-russian/keyword-super
         super.run();
         System.out.println("Hello, World!");
     }
