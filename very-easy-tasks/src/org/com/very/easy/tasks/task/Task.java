0a2ce73bdcb516b29d9f0cb5acee4bfcc0540ee9
Полина <belova.pauline98@gmail.com>
2023-02-11 14:25:45 +0500

    Add comments

--- very-easy-tasks/src/org/com/very/easy/tasks/task/Task.java
+++ very-easy-tasks/src/org/com/very/easy/tasks/task/Task.java
@@ -1,10 +1,16 @@
 package org.com.very.easy.tasks.task;
 
+//создадим абстрактный класс, который будет делать однообразную работу - печатать номер задания и описание сразу после
+//его запуска. Для запуска также напишем метод
+//https://metanit.com/java/tutorial/3.6.php подробнее про абстрактные классы
+
 public abstract class Task {
 
+    //создадим защищенные переменные, чтобы их нельзя было перенаследовать
     protected Integer number;
     protected String description;
 
+    //геттеры и сеттеры для переменных, чтобы ими можно было пользоваться
     public Integer getNumber() {
         return number;
     }
@@ -21,6 +27,7 @@ public abstract class Task {
         this.description = description;
     }
 
+    //метод печатающий номер задачи и ее описание
     public void run() {
         System.out.printf("Задача %d: %s%n", number, description);
     }
